f83ee6d3e1dc8f7039b8f3d8c039b5882d4d499a
R0B3RT1177 <107824315+R0B3RT1177@users.noreply.github.com>
2025-03-05 08:50:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(session({
   secret: 'bingchilling',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true }
+  cookie: { secure: false }
 }));
 
 const path = require("path");
